3ab6759a54f6ee672d6f0fdd8f127d7ecc119758
Cyphe Mercury <cyphe371@gmail.com>
2023-12-06 10:46:01 -0800

    More stats
    Score and accuracy calculation system.
    
    Score system is subject to change.

--- NoteHandler.java
+++ NoteHandler.java
@@ -7,11 +7,13 @@ public class NoteHandler {
   };
   private int score = 0;
   private float accuracy = 1;
+  private int combo = 0;
+  private int maxCombo = 0;
 
   /**
      *
    * @param ms absolute distance in time from being directly on beat with the note
-   * @return 
+   * @return boolean Whether the note was hit or not
    */
   public boolean judgeNote(float ms) {
 
@@ -24,11 +26,27 @@ public class NoteHandler {
     for (Judgment judgment : judgments) {
       if (judgment.getTiming() >= ms) {
         judgment.increaseCount();
+        combo++;
+
+        // If not was a miss, but it was still hit
+        if (judgment.equals(judgments[judgments.length - 1])) {
+          combo = 0;
+          return true;
+        }
+
+        // If current combo is higher than the highest combo, match highest combo with it
+        if (combo > maxCombo) {
+          maxCombo = combo;
+        }
         return true;
       }
     }
 
     return true;
+  }
+
+  public int getCombo() {
+    return maxCombo;
   }
 
   public int calcScore() {
@@ -39,13 +57,13 @@ public class NoteHandler {
       score += judgment.getCount() * judgment.getValue();
     }
 
-    return score;
+    return score * maxCombo;
   }
 
   public float calcAccuracy() {
     // (perfects * 1, goods * 0.5, bads * 0.25) / (totalNotes + missCount)
-    int totalNotes = 0;
-    int weightedAccuracy = 0;
+    float totalNotes = 0;
+    float weightedAccuracy = 0;
 
     for (Judgment judgment : judgments) {
       totalNotes += judgment.getCount();
@@ -53,9 +71,40 @@ public class NoteHandler {
     totalNotes += judgments[judgments.length - 1].getCount();
 
     for (int i = 0; i < judgments.length - 1; i++) {
-      weightedAccuracy += judgments[i].getCount() * (1 / Math.pow(2, i));
+      weightedAccuracy += judgments[i].getCount() * (1.0 / Math.pow(2, i));
     }
 
-    return (float)weightedAccuracy / (float)totalNotes;
+    return weightedAccuracy / totalNotes;
+  }
+
+  public String getJudgmentCount(int index) {
+    String text = "";
+    text += judgments[index].getName() + " Count: " + judgments[index].getCount();
+    return text;
+  }
+
+  public static void main(String[] args) {
+    NoteHandler noteHandler = new NoteHandler();
+    noteHandler.judgeNote(1);
+    noteHandler.judgeNote(2);
+    noteHandler.judgeNote(3);
+    noteHandler.judgeNote(4);
+    noteHandler.judgeNote(5);
+    noteHandler.judgeNote(6);
+    noteHandler.judgeNote(7);
+    noteHandler.judgeNote(8);
+    noteHandler.judgeNote(9);
+    noteHandler.judgeNote(10);
+    noteHandler.judgeNote(11);
+    noteHandler.judgeNote(12);
+    noteHandler.judgeNote(13);
+
+    System.out.println("Score: " + noteHandler.calcScore());
+    System.out.println("Accuracy: " + noteHandler.calcAccuracy() + "%");
+    System.out.println("Combo: " + noteHandler.getCombo() + "x");
+    System.out.println(noteHandler.getJudgmentCount(0));
+    System.out.println(noteHandler.getJudgmentCount(1));
+    System.out.println(noteHandler.getJudgmentCount(2));
+    System.out.println(noteHandler.getJudgmentCount(3));
   }
 }
