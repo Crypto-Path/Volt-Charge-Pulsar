93c8e2929b0b77c10fbc4cfeab52959bfa84c123
Cyphe Mercury <cyphe371@gmail.com>
2023-12-06 10:16:46 -0800

    Judgements for timing ranges

---
+++ Judgment.java
@@ -0,0 +1,45 @@
+public class Judgment {
+    private String name;
+    private int timing;
+    private int value;
+    private int count = 0;
+
+    public Judgment(String name, int timing, int value) {
+      this.name = name;
+      this.timing = timing;
+      this.value = value;
+    }
+
+    public void increaseCount() {
+      this.count++;
+    }
+
+    /**
+     * @return String return the name of the judgement
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return int return the timing range for the judgement
+     */
+    public int getTiming() {
+        return timing;
+    }
+
+    /**
+     * @return int return the timing range for the judgement
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @return int return the number of times the judgement has been met
+     */
+    public int getCount() {
+        return count;
+    }
+
+}
