640c526bc1965c9877b65ea1aeda1897e040d54e
Cyphe Mercury <cyphe371@gmail.com>
2023-12-07 14:48:18 -0800

    Basic note rendering
    Note rendering for the 4 basic notes and long notes and long notes (Doesn't work well with scroll speed).
    
    System for Volts to be implemented later.
    
    Basic testing built in, feel free to mess with the parameters in the main function (this one in this class)

--- Gameplay/NoteRenderer.java
+++ Gameplay/NoteRenderer.java
@@ -1,83 +1,152 @@
 package Gameplay;
 
+import java.awt.Cursor;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.jar.Attributes.Name;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 
 public class NoteRenderer extends JButton {
 
     public JFrame frame;
-    private NoteRenderer[] notes;
+    private int[][][] notes;
+    private static float songPosition = 0;
 
-    public NoteRenderer(JFrame frame) {
+    public NoteRenderer(JFrame frame, int[][][] notes) {
+        setContentAreaFilled(false);
+        setBorderPainted(false);
         this.frame = frame;
+        System.out.println("Initiating Renderer: Retrieved frame");
         this.notes = notes;
+        System.out.println("Initiating Renderer: Retrieved notes");
     }
 
+    @Override
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // Draw note
-        /*
-         * Get note data
-         * Offset note position by audio progress
-         * Get first 3 notes in each direction
-         * Draw rounded square at note position
-         */
-
-        for (NoteRenderer note : notes) {
-            int x = note.getX();
-            int y = note.getY();
-            String direction = note.getDirection();
-
-            // Draw a rounded rectangle for each note
-            g2.setColor(Color.BLUE); // Set the color as needed
-            g2.fillRoundRect(x, y, 30, 30, 10, 10);
-
-            // Draw an arrow or any indicator based on the note direction
-            g2.setColor(Color.BLACK);
-            switch (direction) {
-                case "left":
-                    g2.drawLine(x + 15, y + 15, x + 5, y + 15);
-                    g2.drawLine(x + 5, y + 15, x + 10, y + 10);
-                    g2.drawLine(x + 5, y + 15, x + 10, y + 20);
-                    break;
-                case "right":
-                    g2.drawLine(x + 15, y + 15, x + 25, y + 15);
-                    g2.drawLine(x + 25, y + 15, x + 20, y + 10);
-                    g2.drawLine(x + 25, y + 15, x + 20, y + 20);
-                    break;
-                case "top":
-                    g2.drawLine(x + 15, y + 15, x + 15, y + 5);
-                    g2.drawLine(x + 15, y + 5, x + 10, y + 10);
-                    g2.drawLine(x + 15, y + 5, x + 20, y + 10);
-                    break;
-                case "bottom":
-                    g2.drawLine(x + 15, y + 15, x + 15, y + 25);
-                    g2.drawLine(x + 15, y + 25, x + 10, y + 20);
-                    g2.drawLine(x + 15, y + 25, x + 20, y + 20);
-                    break;
-                default:
-                    // Handle other directions or provide a default indicator
+        for (int laneIndex = 0; laneIndex < notes.length; laneIndex++) {
+            int[][] lane = notes[laneIndex];
+            for (int noteIndex = 0; noteIndex < lane.length; noteIndex++) {
+                int[] note = lane[noteIndex];
+                int position = note[0];
+                int longness = note[1];
+                DrawNote(g2, position, longness, laneIndex);
             }
         }
 
         super.paintComponent(g);
     }
 
-    private String getDirection() {
-        return null;
+    private void DrawNote(Graphics2D g2, float position, int length, int direction) {
+        int scrollSpeed = 10;
+
+        int sizeX = 30;
+        int sizeY = 30;
+        int sizeOffsetX = sizeX/2;
+        int sizeOffsetY = sizeY/2;
+
+        int x = 0;
+        int y = 0;
+
+        int drawPosition = (int)((position - songPosition) * scrollSpeed);
+        if (drawPosition < -30 * length) return;
+        int centerPositionX = frame.getWidth() / 2 - sizeOffsetX;
+        int centerPositionY = frame.getHeight() / 2 - sizeOffsetY;
+
+        switch (direction) {
+            case 0: // Up
+                x = centerPositionX;
+                y = centerPositionY - drawPosition - 30;
+                sizeY *= length;
+                if (drawPosition < 30 * (length - 1) && length > 1) sizeY += drawPosition - 30 * (length - 1);
+                break;
+            case 1: // Down
+                x = centerPositionX;
+                y = centerPositionY + drawPosition - 30 * (length - 2);
+                sizeY *= length;
+                drawPosition -= 30 * (length - 1);
+                if (drawPosition < 0 && length > 1) {
+                    sizeY += drawPosition;
+                    y -= drawPosition;
+                }
+                break;
+            case 2: // Left
+                x = centerPositionX - drawPosition - 30;
+                y = centerPositionY;
+                sizeX *= length;
+                if (drawPosition < 30 * (length - 1) && length > 1) sizeX += drawPosition - 30 * (length - 1);
+                break;
+            case 3: // Right
+                x = centerPositionX + drawPosition - 30 * (length - 2);
+                y = centerPositionY;
+                sizeX *= length;
+                drawPosition -= 30 * (length - 1);
+                if (drawPosition < 0 && length > 1) {
+                    sizeX += drawPosition;
+                    x -= drawPosition;
+                }
+                break;
+        
+            default:
+                break;
+        }
+
+        g2.fillRoundRect(x, y, sizeX, sizeY, 10, 10);
     }
 
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Initiating Frame");
+                JFrame frame = new JFrame("Note renderer");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(800, 600);
+                frame.setLocationRelativeTo(null);
+                frame.setLayout(null);
+                System.out.println("Finalizing Frame");
 
+                System.out.println("Initiating Renderer");
+                NoteRenderer noteRenderer = new NoteRenderer(frame, new int[][][] {
+                    /* Lane 3 */ {{14, 1}, {15, 1}, {20, 2}, {24, 2}},
+                    /* Lane 1 */ {{20, 2}},
+                    /* Lane 2 */ {{20, 2}},
+                    /* Lane 4 */ {{20, 2}}
+                });
+                frame.add(noteRenderer);
+                noteRenderer.setBounds(0, 0, frame.getWidth(), frame.getHeight());
+                System.out.println("Finalizing Renderer");
+
+                frame.setVisible(true);
+
+                // Emulates audio progress playing
+                Timer timer = new Timer(20, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        songPosition += 0.16;
+                        frame.repaint();
+                    }
+                }); 
+                timer.start();
+            }
+        });
     }
 }
