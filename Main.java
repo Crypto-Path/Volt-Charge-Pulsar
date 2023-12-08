90671153f0ef18742608f6b088b8202923793b1f
Cyphe Mercury <cyphe371@gmail.com>
2023-12-08 10:45:29 -0800

    Now relies on a scenemanager

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import java.util.*;
+
 public class Main {
     static JFrame frame = new JFrame("Volt Charge Pulsar");
 
@@ -13,13 +15,9 @@ public class Main {
         JPanel panel = new JPanel();
         frame.add(panel);
 
-        // Load main menu
-        Menu menu = new Menu(frame);
-        menu.initMenu();
-        menu.initGUI();
+        SceneManager sceneManager = new SceneManager(frame);
+        sceneManager.LoadMenu();
 
-        // Load user input
-        KeyBindings keyBindings = new KeyBindings(menu);
-        frame.addKeyListener(keyBindings.getBindings());
+        frame.repaint();
     }
 }
